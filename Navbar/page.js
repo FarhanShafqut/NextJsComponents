3c57e32198a180b3dfcba1fcf89b3eb91f4ef298
FarhanShafqut <farhanshafqut869@gmail.com>
2023-08-20 12:09:48 +0500

    nav complete

--- Navbar/page.js
+++ Navbar/page.js
@@ -7,7 +7,7 @@ import {AiOutlineMenu,AiOutlineClose} from 'react-icons/ai'
 export default function Navbar() {
   const [nav, Setnav] = useState(false)
   const [color, Setcolor] = useState('transparent')
-  const [textcolor, Settextcolor] = useState('white')
+  const [textcolor, Settextcolor] = useState('Green')
 
 const handlenav = () =>{
   Setnav(!nav)
@@ -15,25 +15,26 @@ const handlenav = () =>{
 
 useEffect(()=>{
   const changeColor = () => {
-    if (window.scroll >=90 ){
+    if (window.scrollY >=90 ){
       Setcolor(`#ffffff`);
       Settextcolor(`#000000`);
     }
     else{
       Setcolor('transparent');
-      Settextcolor(`#ffffff`);
+      Settextcolor(`Green`);
     }
   }; window.addEventListener('scroll',changeColor)
 }
 ,[]);
   return (
     // fixed left-0 top-0  
-    <div style={{backgroundColor : `${color}`}} className='sticky z-10 ease-in duration-300  ' >
+    <div>
+    <div style={{backgroundColor : `${color}`}} className='fixed left-0 top-0  w-full z-10 ease-in duration-300  ' >
         <div className='max-w-[1240px]  m-auto flex justify-between items-center p-4  :'>
           <Link href="./">
-           <h1 style={{Color : `${textcolor}`}}  className='font-semibold text-lg' >Navbar</h1>
+           <h1 style={{color : `${textcolor}`}}  className='font-semibold text-lg' >Navbar</h1>
           </Link>
-        <ul style={{Color : `${textcolor}`}} className='hidden sm:flex' >
+        <ul style={{color : `${textcolor}`}} className='hidden sm:flex' >
           <li Link href='./' className='p-4'> Home</li>
           <li Link href='./' className='p-4'> About</li>
           <li Link href='./' className='p-4'> Contact us</li>
@@ -41,7 +42,7 @@ useEffect(()=>{
         </ul>
           {/* Mobile Button  */}
           <div onClick={handlenav}   className='block sm:hidden z-10'>
-            {nav ? <AiOutlineClose size={20} style={{Color : `${textcolor}`}} />  :  <AiOutlineMenu size={20} style={{Color : `${textcolor}`}} />}
+            {nav ? <AiOutlineClose size={20} style={{color : `${textcolor}`}} />  :  <AiOutlineMenu size={20} style={{color : `${textcolor}`}} />}
           </div>
           {/* Mobile menu  */}
           <div className={nav ? 'sm:hidden absolute top-0 left-0 bottom-0 right-0 flex justify-center items-center w-full h-screen bg-black text-white ease-in duration-300 ' : 'sm:hidden absolute top-0 left-[-100%] bottom-0 right-0 flex justify-center items-center w-full h-screen bg-black text-white ease-in duration-300 '}>
@@ -53,7 +54,7 @@ useEffect(()=>{
           </ul>
           </div>
         </div>
-       
+        </div>
     </div>
   )
 }
